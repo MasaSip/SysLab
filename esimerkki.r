d29cacfe7de358dc66ad8b16664880745cf0ef43
Masa <matti.sippola@aalto.fi>
2016-04-21 17:33:58 +0300

    Add to Dropbox

--- esimerkki.r
+++ esimerkki.r
@@ -1,21 +1,21 @@
 #Esimerkki useamman kausidifferoinnin (viipeet L1 ja L2) mallin rakentamisesta
 
-#Luetaan s‰hkˆnkulutus- ja l‰mpˆtiladata, hyp‰t‰‰n headerrivin yli
+#Luetaan s√§hk√∂nkulutus- ja l√§mp√∂tiladata, hyp√§t√§√§n headerrivin yli
 eletemp = read.table(file = "sahko.csv", sep = ";", dec = ",", skip = 1)
 
-#S‰hkˆnkulutus aikasarjaksi
+#S√§hk√∂nkulutus aikasarjaksi
 ele = ts(eletemp[[1]][1:816], start = 1, frequency = 24)
 
 L1 = 168
 L2 = 24
-#Ensimm‰inen kausidifferointi L1:ll‰
+#Ensimm√§inen kausidifferointi L1:ll√§
 dele168 = diff(ele, lag = L1, differences = 1)
 
 #Estimoidaan SARIMA-malli kausivaihtelun jaksonpituudella L2, mallin muut parametrit vedetty hatusta
 malli = arima(dele168, order = c(1,1,1), seasonal = list(order = c(1,1,1), period = L2), method = "CSS")
 enne = predict(malli, n.ahead = 24)
 
-#Integroidaan for-loopilla ennuste ja luottamusv‰lit kausivaihtelun pituudella L1
+#Integroidaan for-loopilla ennuste ja luottamusv√§lit kausivaihtelun pituudella L1
 ennuste = c(1:24)
 clyla = c(1:24)
 clala = c(1:24)
@@ -25,7 +25,7 @@ for (x in c(1:24)) {
 	clala[x] = ennuste[x] - 1.96*enne$se[x]
 }
 
-#Tehd‰‰n ennusteesta aikasarja ja plotataan
+#Tehd√§√§n ennusteesta aikasarja ja plotataan
 ennuste = ts(ennuste, start = c(35,1), frequency = 24)
 clyla = ts(clyla, start = c(35,1), frequency = 24)
 clala = ts(clala, start = c(35,1), frequency = 24)
--- pohja.r
+++ pohja.r
@@ -1,32 +1,41 @@
+#install.packages('forecast')
+library(forecast)
 
-#setwd('Z:/Documents') #RStudiossa t‰m‰n voi tehd‰ myˆs valikosta Session->Set working directory
-#Luetaan s‰hkˆnkulutus- ja l‰mpˆÜtiladata, hyp‰t‰‰nn headerrivin yli
+#PI: Automaattinen ratkaisin?
+aele=auto.arima(ele)
+
+
+
+#setwd('Z:/Documents') #RStudiossa t?m?n voi tehd? my?s valikosta Session->Set working directory
+#Luetaan s?hk?nkulutus- ja l?mp??tiladata, hyp?t??nn headerrivin yli
 eletemp = read.table(file = "sahko.csv", sep = ";", dec = ",", skip = 1)
 
-#S‰hkˆnkulutus aikasarjaksi
+#S?hk?nkulutus aikasarjaksi
 ele = ts(eletemp[[1]][1:816], start = 1, frequency = 24)
 
-#L‰mpˆtila kahdeksi aikasarjaksi: 816 ensimm‰ist‰ havaintoa k‰ytet‰‰n mallin estimointiin ja 24 viimeist‰ havaintoa ennustamiseen.
+#L?mp?tila kahdeksi aikasarjaksi: 816 ensimm?ist? havaintoa k?ytet??n mallin estimointiin ja 24 viimeist? havaintoa ennustamiseen.
 temp = ts(eletemp[[2]], start = 1, frequency = 24)
 temp816 = ts(eletemp[[2]][1:816], start = 1, frequency = 24)
-temp24 = ts(eletemp[[2]][817:840], start = c(35,1), frequency = 24) #start parametrina vektori: 817. tunti = 35. p‰iv‰n ensimm‰inen tunti
+temp24 = ts(eletemp[[2]][817:840], start = c(35,1), frequency = 24) #start parametrina vektori: 817. tunti = 35. p?iv?n ensimm?inen tunti
 
 #Plotataan aikasarjat
 ts.plot(ele, gpars=list(xlab="aika/vrk", ylab = "kulutus/kWh"))
 ts.plot(temp816,temp24, gpars=list(xlab="aika/vrk", ylab=expression(~degree~C), col = c("black", "blue")))
 
 #Plotataan autokorrelaatio-, osittaisautokorrelaatio- ja ristikorrelaatiofunktiot.
-par(mfrow=c(2,2)) 				#jos haluat katsoa kuvia 2x2 matriisin sijaan yksitellen, laita t‰m‰ rivi kommentteihin (#-merkki)
+par(mfrow=c(2,2)) 				#jos haluat katsoa kuvia 2x2 matriisin sijaan yksitellen, laita t?m? rivi kommentteihin (#-merkki)
 acf(ele, lag.max=168)
 acf(ele, lag.max=168, type = "partial")
 acf(temp, lag.max=168)
 acf(temp, lag.max=168, type = "partial")
+
+par(mfrow=c(1,1))
 ccf(ele,temp, lag.max=168)
 
-#Stationarisoidaan aikasarjat. Huomaa, ett‰ s‰hkˆnkulutuksen ja l‰mpˆtilan aikasarjojen differointien asteiden ei v‰ltt‰m‰tt‰ tarvitse olla samoja. Sijoita differoinnin kertaluku d, kausidifferoinnin jakso S, kausidifferensoinnin kertaluku D ja poista rivit 24-28 kommenteista.
-#d = ?
-#S = ?
-#D = ?
+#Stationarisoidaan aikasarjat. Huomaa, ett? s?hk?nkulutuksen ja l?mp?tilan aikasarjojen differointien asteiden ei v?ltt?m?tt? tarvitse olla samoja. Sijoita differoinnin kertaluku d, kausidifferoinnin jakso S, kausidifferensoinnin kertaluku D ja poista rivit 24-28 kommenteista.
+d = 1
+S = 24
+D = 0
 dele = ele
 dtemp = temp
 if (d > 0) {
@@ -38,22 +47,27 @@ dele = diff(dele, lag = S, differences = D)
 dtemp = diff(dtemp, lag = S, differences = D)
 }
 
-#Differoitujen aikasarjojen autokorrelaatio-, osittaisautokorrelaatio- ja ristikorrelaatiofunktiot. Poista rivit 31-34 kommenteista, kun dele ja dtemp on m‰‰r‰tty.
-#acf(dele, lag.max=168)
-#acf(dele, lag.max=168, type = "partial")
-#acf(dtemp, lag.max=168)
-#acf(dtemp, lag.max=168, type = "partial")
-#ccf(dele, dtemp, lag.max=168)
+#Differoitujen aikasarjojen autokorrelaatio-, osittaisautokorrelaatio- ja ristikorrelaatiofunktiot. Poista rivit 31-34 kommenteista, kun dele ja dtemp on m??r?tty.
+par(mfrow=c(2,2))
+acf(dele, lag.max=168)
+acf(dele, lag.max=168, type = "partial")
+acf(dtemp, lag.max=168)
+acf(dtemp, lag.max=168, type = "partial")
+par(mfrow=c(1,1))
+ccf(dele, dtemp, lag.max=168)
 
-#Estimoidaan malli ja lasketaan ennusteet ilman ulkoista muuttujaa. M‰‰r‰‰ mallin parametrit p, q, P ja Q ja poista #-merkit riveilt‰ 37-42.
-#p = ?
-#q = ?
-#P = ?
-#Q = ?
-#malli = arima(ele, order = c(p,d,q), seasonal = list(order = c(P, D, Q), period = S), method = "CSS")
-#enne = predict(malli, n.ahead = 24)
+ts.plot(dele)
+ts.plot(dtemp)
 
-#Estimoidaan malli l‰mpˆtilan kanssa. M‰‰r‰‰ l‰mpˆtilan mahdollinen viive L. Poista #-merkit riveilt‰ 47-52.
+#Estimoidaan malli ja lasketaan ennusteet ilman ulkoista muuttujaa. M??r?? mallin parametrit p, q, P ja Q ja poista #-merkit riveilt? 37-42.
+p = 3
+q = 0
+P = 2
+Q = 0
+malli = arima(ele, order = c(p,d,q), seasonal = list(order = c(P, D, Q), period = S), method = "CSS")
+enne = predict(malli, n.ahead = 24)
+
+#Estimoidaan malli l?mp?tilan kanssa. M??r?? l?mp?tilan mahdollinen viive L. Poista #-merkit riveilt? 47-52.
 #L = ?
 #tempestimointi = eletemp[[2]][1:(816-L)]
 #tempennuste = eletemp[[2]][(816-L+1):(816-L+24)]
@@ -61,17 +75,17 @@ dtemp = diff(dtemp, lag = S, differences = D)
 #malli2 = arima(eleestimointi, order = c(p,d,q), seasonal = list(order = c(P, D, Q), period = S), xreg = tempestimointi, method = "CSS")
 #enne2 = predict(malli2, n.ahead = 24, newxreg = tempennuste)
 
-#Esimerkki Portmanteau-testist‰. Poista #-merkki. Onko residuaaliaikasarjan alussa nollia?
+#Esimerkki Portmanteau-testist?. Poista #-merkki. Onko residuaaliaikasarjan alussa nollia?
 #Box.test(malli$residuals, lag = 20, type = "Ljung-Box", fitdf = p + q + P + Q)
 
-par(mfrow=c(1,1))
+#par(mfrow=c(1,1))
 
-#Plotataan kuva sahkonkulutusaikasarjasta, mallin (1) sovitteesta, ennusteesta ja ennusteen 95 %:n eli 1,96*sigma-luottamusv‰leist‰. Poista #-merkki.
-#ts.plot(ele, ele - malli$residuals, enne$pred, enne$pred + 1.96*enne$se, enne$pred - 1.96*enne$se, col = c("black", "red", "blue", "blue", "blue"), main  = "Sovite ja ennuste")
+#Plotataan kuva sahkonkulutusaikasarjasta, mallin (1) sovitteesta, ennusteesta ja ennusteen 95 %:n eli 1,96*sigma-luottamusv?leist?. Poista #-merkki.
+ts.plot(ele, ele - malli$residuals, enne$pred, enne$pred + 1.96*enne$se, enne$pred - 1.96*enne$se, col = c("black", "red", "blue", "blue", "blue"), main  = "Sovite ja ennuste")
 
-#Plotataan kuva pelk‰st‰ ennusteesta. Poista #-merkki.
-#ts.plot(enne$pred, enne$pred + 1.96*enne$se, enne$pred - 1.96*enne$se, col = c("black", "blue", "blue"), main = "Ennuste ja  95 %:n luottamusv‰lit")
+#Plotataan kuva pelk?st? ennusteesta. Poista #-merkki.
+#ts.plot(enne$pred, enne$pred + 1.96*enne$se, enne$pred - 1.96*enne$se, col = c("black", "blue", "blue"), main = "Ennuste ja  95 %:n luottamusv?lit")
 
-#Kirjoitetaan ennuste ja luottamusv‰lit .csv-tiedostoon, jonka voi avata Excelill‰. Poista #-merkit riveilt‰ 66-67.
+#Kirjoitetaan ennuste ja luottamusv?lit .csv-tiedostoon, jonka voi avata Excelill?. Poista #-merkit riveilt? 66-67.
 #output = cbind(enne$pred, enne$pred + 1.96*enne$se, enne$pred - 1.96*enne$se)
 #write.csv2(output, file = "ennuste.csv")
